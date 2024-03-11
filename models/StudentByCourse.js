25074909b81dc275ff6bc6f9dbe9d15a407e51c8
Tomas Apochian <tomasapochian@gmail.com>
2024-03-10 22:29:37 -0300

    add StudentByCourse pivot table for matching students with the courses they participated in

---
+++ models/StudentByCourse.js
@@ -0,0 +1,14 @@
+const db = require("../config/db")
+const { Model } = require('sequelize');
+
+class StudentByCourse extends Model { }
+
+StudentByCourse.init({}, {
+    sequelize: db,
+    modelName: 'student_by_course',
+    timestamps : false,
+    freezeTableName : true
+});
+
+
+module.exports = StudentByCourse

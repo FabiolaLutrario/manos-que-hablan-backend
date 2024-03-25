eaba8f5e1309292781bfeea8d9c3662910cf597c
Tomas Apochian <tomasapochian@gmail.com>
2024-03-24 22:46:09 -0300

    add editById student method

--- routes/Student.routes.js
+++ routes/Student.routes.js
@@ -5,6 +5,8 @@ const StudentController = require("../controllers/Student.controller")
 StudentRouter.get("/", StudentController.getAll)
 StudentRouter.get("/:id", StudentController.getById)
 
+StudentRouter.put('/:id', StudentController.editById)
+
 
 module.exports = StudentRouter
 
